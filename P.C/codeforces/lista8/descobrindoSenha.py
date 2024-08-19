9f2f659ee56417182b5d07f9861e898f38a01d8c
Lucas Vinícius Teixeira de Góis <20241014040015@ifrn.local>
2024-08-19 09:47:32 -0300

    feat: finalizando lista 8 codeforces

--- P.C/codeforces/lista8/descobrindoSenha.py
+++ P.C/codeforces/lista8/descobrindoSenha.py
@@ -1,6 +1,5 @@
 
 def func():
-    copiaL = l.copy()
     ln = ''
 
     for x in range(0, qtd):
@@ -8,18 +7,13 @@ def func():
         for i in range(0,len(l)):
             if l[i]>m:
                 m = l[i]
+
             else:
                 m = m
 
-        if str(copiaL.index(m)) in ln:
-            for x in range(len(copiaL)-1,0-1,-1):
-                if m == copiaL[x]:
-                    k = x
-                    break
-            ln += (str(k))
-        else:
-            ln += (str(copiaL.index(m)))
-        l.remove(m)
+        ln += (str(l.index(m)))
+        l[l.index(m)] = -1
+
     return ln
     
 resultado = []
@@ -27,10 +21,10 @@ while True:
     try:
         qtd = int(input())
         l = list(map(float,input().split()))
+
         resultado.append(func())
     except EOFError:
         break
     
 for x in range(0, len(resultado)):
     print("Caso {}: {}".format(x+1, resultado[x]))
-
